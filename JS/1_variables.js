e3eb6f2f17ddcd9111e0096e777aa59bad71705d
Mohit Chaudhary <117006118+m0hitchaudhary@users.noreply.github.com>
2024-02-03 20:14:40 +0000

    datatypes & variables

---
+++ JS/1_variables.js
@@ -0,0 +1,16 @@
+const accountId = 3213
+let accountEmail = "mohit@gmail.com"
+var accountPass = "12344"
+accountCity = "Punjab" //this can be used but shouldn't be used
+let accState
+//if we just declare a variable n don't define it then it's value if undefined by default
+// accountId = 31421
+
+// console.log(accountId); //it is not valid, we cannot change the value of const variable
+
+accountEmail = 'mohit@yahoo.com'
+accountPass = '3431'
+accountCity = "Delhi"
+
+console.table([accountId, accountEmail, accountPass, accountCity, accState])
+//the above code line is used print the entire content of in the form of a table
---
+++ JS/2_datatypes.js
@@ -0,0 +1,16 @@
+"use strict" //it is keyword which makes sure that all the code written is as per new standards, but we don't need to write this bc of the advancements in language
+
+// alert(6+6) //this syntax of alert is used when we bind JS with HTML, it returns error in node
+
+let name = "Mohit"
+let age = 19
+let isLoggedIn = false
+let state = null
+
+//null => gives standalone value
+//undefined => means that the value has not been assigned
+//symbol => uniquely indentfy elements, will work with this in REACT
+
+
+console.log(typeof null) //returns object
+console.log(typeof undefined) //returns undefined
